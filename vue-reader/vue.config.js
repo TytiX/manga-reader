2fdf052bd56c5dcc8a9dec5f31b1aaa466dc03cb
Antoine Belliard <tsi.belliard@gmail.com>
2020-06-22 12:31:27 +0200

    Add vue install pwa

--- vue-reader/vue.config.js
+++ vue-reader/vue.config.js
@@ -7,5 +7,20 @@ module.exports = {
         changeOrigin: true
       }
     }
+  },
+  pwa: {
+    name: 'Manga Reader',
+    themeColor: '#343a40',
+    msTileColor: '#343a40',
+    appleMobileWebAppCapable: 'yes',
+    appleMobileWebAppStatusBarStyle: 'black',
+
+    // configure the workbox plugin
+    workboxPluginMode: 'InjectManifest',
+    workboxOptions: {
+      // swSrc is required in InjectManifest mode.
+      swSrc: 'dev/sw.js',
+      // ...other Workbox options...
+    }
   }
 };
