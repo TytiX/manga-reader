a45e57fca6b3b7d676f3dbfcefc1af25755b5e3c
Antoine Belliard <tsi.belliard@gmail.com>
2020-07-02 14:44:02 +0200

    migrate to arrow function

--- node-server/routes/ChapterRoutes.ts
+++ node-server/routes/ChapterRoutes.ts
@@ -6,7 +6,7 @@ import { scanChapterPages } from '../scanners/scanner-store';
 export default (db: Database) => {
   const router = Router();
 
-  router.get('/orphans', async function(req, res) {
+  router.get('/orphans', async (req, res) => {
     res.send(
       await db.chapterRepository.findAndCount({
         relations: [ 'source' ],
@@ -16,7 +16,7 @@ export default (db: Database) => {
       })
     );
   });
-  router.delete('/orphans', async function(req, res) {
+  router.delete('/orphans', async (req, res) => {
     const orphansChapters = await db.chapterRepository.find({
       relations: [ 'source' ],
       where: {
@@ -28,29 +28,29 @@ export default (db: Database) => {
       await db.chapterRepository.delete(orphansChapters.map(c => c.id))
     );
   });
-  router.get('/:id', async function(req, res) {
+  router.get('/:id', async (req, res) => {
     const chapter = await db.findChapterById(req.params.id)
     res.send( {
       ...chapter,
       pages: chapter.pages
     } );
   });
-  router.get('/:id/next', async function(req, res) {
+  router.get('/:id/next', async (req, res) => {
     res.send(
       await db.findNextChapterById(req.params.id)
     );
   });
-  router.get('/:id/previous', async function(req, res) {
+  router.get('/:id/previous', async (req, res) => {
     res.send(
       await db.findPreviousChapterById(req.params.id)
     );
   });
-  router.post('/', async function(req, res) {
+  router.post('/', async (req, res) => {
     res.send(
       await db.findChapterByLink((req.body as any).link)
     );
   });
-  router.post('/scan', async function(req, res) {
+  router.post('/scan', async (req, res) => {
     scanChapterPages(req.body);
     res.send({
       status: 'running'
