617076048894a8c3d4ca6bada7f638fb2853ad97
Belliard Antoine <antoine.belliard@soprasteria.com>
2020-07-07 12:00:07 +0200

    Try new workbox config

--- vue-reader/public/service-worker.ts
+++ vue-reader/public/service-worker.ts
@@ -2,36 +2,43 @@ self.addEventListener('push', ev => {
   const data = ev.data.json();
   self.registration.showNotification(data.title, data.options);
 });
+/***************************************************************
+ * WORKBOX
+ ***************************************************************/
+workbox.setConfig({
+  debug: false,
+});
 
-workbox.core.setCacheNameDetails({ prefix: 'd4' })
-//Change this value every time before you build
-const LATEST_VERSION = 'v0.1'
-self.addEventListener('activate', (event) => {
-  console.log(`%c ${LATEST_VERSION} `, 'background: #ddd; color: #0000ff')
-  if (caches) {
-    caches.keys().then((arr) => {
-      arr.forEach((key) => {
-        if (key.indexOf('d4-precache') < -1) {
-          caches.delete(key).then(() => console.log(`%c Cleared ${key}`, 'background: #333; color: #ff0000'))
-        } else {
-          caches.open(key).then((cache) => {
-            cache.match('version').then((res) => {
-              if (!res) {
-                cache.put('version', new Response(LATEST_VERSION, { status: 200, statusText: LATEST_VERSION }))
-              } else if (res.statusText !== LATEST_VERSION) {
-                caches.delete(key).then(() => console.log(`%c Cleared Cache ${LATEST_VERSION}`, 'background: #333; color: #ff0000'))
-              } else console.log(`%c Great you have the latest version ${LATEST_VERSION}`, 'background: #333; color: #00ff00')
-            })
-          })
-        }
-      })
-    })
-  }
-})
+workbox.precaching.precacheAndRoute([]);
 
-workbox.skipWaiting()
-workbox.clientsClaim()
+workbox.routing.registerRoute(
+  /\.(?:png|gif|jpg|jpeg|svg)$/,
+  workbox.strategies.staleWhileRevalidate({
+    cacheName: 'images',
+    plugins: [
+      new workbox.expiration.Plugin({
+        maxEntries: 120,
+        maxAgeSeconds: 30 * 24 * 60 * 60, // 30 Days
+      }),
+    ],
+  }),
+);
 
-self.__precacheManifest = [].concat(self.__precacheManifest || [])
-workbox.precaching.suppressWarnings()
-workbox.precaching.precacheAndRoute(self.__precacheManifest, {})
+workbox.routing.registerRoute(
+  new RegExp('http://172.22.22.52:3000/api'),
+  workbox.strategies.networkFirst({
+    cacheName: 'api',
+  }),
+);
+
+workbox.routing.registerRoute(
+  new RegExp('http://172.22.22.52:3000'),
+  workbox.strategies.cacheFirst({
+    cacheName: 'app',
+    plugins: [
+      new workbox.expiration.Plugin({
+        maxEntries: 30,
+      }),
+    ],
+  }),
+);
