86180982871e9e18eaa72663d3a95a252aefef7e
Antoine Belliard <tsi.belliard@gmail.com>
2020-11-09 19:51:17 +0100

    Update ImageRoutes.ts

--- node-server/routes/ImageRoutes.ts
+++ node-server/routes/ImageRoutes.ts
@@ -2,11 +2,13 @@ import { Router } from 'express';
 
 import { Database } from '../database/Database';
 import axios from 'axios';
+import logger from '../logger';
 
 export default (db: Database) => {
   const router = Router();
 
   router.get('/fromUrl/:url', async (req, res) => {
+    logger.debug(`ImageAPI --> get image from ${req.params.url}`);
     try {
       const response = await axios.get(
         req.params.url,
@@ -19,6 +21,7 @@ export default (db: Database) => {
   });
 
   router.post('/fromUrl', async (req, res) => {
+    logger.debug(`ImageAPI --> psot image from ${req.body.url}`);
     try {
       const response = await axios.get(
         req.body.url,
@@ -31,6 +34,7 @@ export default (db: Database) => {
   });
 
   router.get('/:chapterId/:pageNumber', async (req, res) => {
+    logger.debug(`ImageAPI --> get image from ${req.params.chapterId}, page ${req.params.pageNumber}`);
     try {
       const chapter = await db.findChapterById(req.params.chapterId);
       const response = await axios.get(
