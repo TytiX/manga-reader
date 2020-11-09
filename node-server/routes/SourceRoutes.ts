60bfb341a1684795f041ce338b1180aaaebb888d
Antoine Belliard <tsi.belliard@gmail.com>
2020-11-09 19:44:37 +0100

    Update SourceRoutes.ts

--- node-server/routes/SourceRoutes.ts
+++ node-server/routes/SourceRoutes.ts
@@ -1,11 +1,13 @@
 import { Router } from 'express';
 
 import { Database } from '../database/Database';
+import logger from '../logger';
 
 export default (db: Database) => {
   const router = Router();
 
   router.get('/orflans', async function(req, res) {
+    logger.debug(`SourcesAPI --> get orflans manga source`);
     res.send(
       await db.sourceRepository.findAndCount({
         relations: [ 'manga' ],
@@ -16,11 +18,13 @@ export default (db: Database) => {
     );
   });
   router.get('/:id', async function(req, res) {
+    logger.debug(`SourcesAPI --> get source ${req.params.id}`);
     res.send(
       await db.findSourceById(req.params.id)
     );
   });
   router.post('/:id/readmode', async function(req, res) {
+    logger.debug(`SourcesAPI --> change source readmode ${req.params.id} to ${req.body.mode}`);
     res.send(
       await db.updateReadMode(req.params.id, req.body.mode)
     );
