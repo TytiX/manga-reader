15224b0f5169ee7ee4495d86c47fdb1c3ee44f8d
Antoine Belliard <tsi.belliard@gmail.com>
2020-11-09 19:33:49 +0100

    update tag logging

--- node-server/routes/TagRoutes.ts
+++ node-server/routes/TagRoutes.ts
@@ -3,21 +3,25 @@ import { Router } from 'express';
 import { Database } from '../database/Database';
 import { IsNull } from 'typeorm';
 import { Tag } from '../database/entity/Tag';
+import logger from '../logger';
 
 export default (db: Database) => {
   const router = Router();
 
   router.get('/', async (req, res) => {
+    logger.debug(`TagsAPI --> get All tags`);
     res.send(
       await db.allTags()
      );
   });
   router.get('/values', async (req, res) => {
+    logger.debug(`TagsAPI --> get All tags values`);
     res.send(
       await db.findTags()
      );
   });
   router.post('/', async (req, res) => {
+    logger.debug(`TagsAPI --> create All tags ${req.body}`);
     const tags: Tag[] = req.body;
     for (const tag of tags) {
       await db.tagRepository.save(tag);
@@ -25,6 +29,7 @@ export default (db: Database) => {
     res.send( {status: 'ok'} );
   });
   router.get('/cleanup', async (req, res) => {
+    logger.debug(`TagsAPI --> tag cleanup`);
     const tags = await db.findTags();
     for (const tag of tags) {
       if (tag.values.length === 0) {
