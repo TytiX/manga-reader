36d4562db5bca4273acc9edac5452bb8b186c29d
Antoine Belliard <tsi.belliard@gmail.com>
2020-11-09 19:51:11 +0100

    Update ChapterRoutes.ts

--- node-server/routes/ChapterRoutes.ts
+++ node-server/routes/ChapterRoutes.ts
@@ -1,12 +1,14 @@
 import { Router } from 'express';
 
 import { Database } from '../database/Database';
+import logger from '../logger';
 import { scanChapterPages } from '../scanners/scanner-store';
 
 export default (db: Database) => {
   const router = Router();
 
   router.get('/orphans', async (req, res) => {
+    logger.debug(`ChapterAPI --> orphans chapters`);
     res.send(
       await db.chapterRepository.findAndCount({
         relations: [ 'source' ],
@@ -17,6 +19,7 @@ export default (db: Database) => {
     );
   });
   router.delete('/orphans', async (req, res) => {
+    logger.debug(`ChapterAPI --> delete orphans chapters`);
     const orphansChapters = await db.chapterRepository.find({
       relations: [ 'source' ],
       where: {
@@ -29,6 +32,7 @@ export default (db: Database) => {
     );
   });
   router.get('/:id', async (req, res) => {
+    logger.debug(`ChapterAPI --> get chapters ${req.params.id}`);
     const chapter = await db.findChapterById(req.params.id)
     res.send( {
       ...chapter,
@@ -36,21 +40,25 @@ export default (db: Database) => {
     } );
   });
   router.get('/:id/next', async (req, res) => {
+    logger.debug(`ChapterAPI --> get next chapters ${req.params.id}`);
     res.send(
       await db.findNextChapterById(req.params.id)
     );
   });
   router.get('/:id/previous', async (req, res) => {
+    logger.debug(`ChapterAPI --> get previous chapters ${req.params.id}`);
     res.send(
       await db.findPreviousChapterById(req.params.id)
     );
   });
   router.post('/', async (req, res) => {
+    logger.debug(`ChapterAPI --> find chapters ${(req.body as any).link}`);
     res.send(
       await db.findChapterByLink((req.body as any).link)
     );
   });
   router.post('/scan', async (req, res) => {
+    logger.debug(`ChapterAPI --> scan chapters ${req.body}`);
     scanChapterPages(req.body);
     res.send({
       status: 'running'
