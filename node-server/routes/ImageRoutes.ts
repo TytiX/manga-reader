8f68a374750113f347ec49e130554007b3bf6ef8
Antoine Belliard <tsi.belliard@gmail.com>
2020-12-03 22:53:48 +0100

    add cache to server

--- node-server/routes/ImageRoutes.ts
+++ node-server/routes/ImageRoutes.ts
@@ -3,6 +3,18 @@ import { Router } from 'express';
 import { http } from '../utils/Http';
 import { Database } from '../database/Database';
 import logger from '../logger';
+import cache from '../utils/cache';
+
+async function getImage(url: string) {
+  const key = url;
+  if (cache.exists(key)) {
+    return cache.get(key)
+  } else {
+    const response = await http.stream(url);
+    cache.put(key, response);
+    return response;
+  }
+}
 
 export default (db: Database) => {
   const router = Router();
@@ -10,18 +22,18 @@ export default (db: Database) => {
   router.get('/fromUrl/:url', async (req, res) => {
     logger.debug(`ImageAPI --> get image from ${req.params.url}`);
     try {
-      const response = await http.stream(req.params.url);
-      response.pipe(res);
+      const img = await getImage(req.params.url)
+      img.pipe(res);
     } catch (e) {
       res.status(500).send(e)
     }
   });
 
   router.post('/fromUrl', async (req, res) => {
-    logger.debug(`ImageAPI --> psot image from ${req.body.url}`);
+    logger.debug(`ImageAPI --> post image from ${req.body.url}`);
     try {
-      const response = await http.stream(req.body.url);
-      response.pipe(res);
+      const img = await getImage(req.body.url)
+      img.pipe(res);
     } catch (e) {
       res.status(500).send(e)
     }
---
+++ node-server/utils/cache.ts
@@ -0,0 +1,57 @@
+import cron from 'node-cron';
+import fs from 'fs';
+import path from 'path';
+import { Readable } from 'stream';
+import moment from 'moment';
+import logger from '../logger';
+
+const CACHE_DIR = path.join('.', 'data', 'tmp');
+
+class CacheManager {
+  cleanup;
+
+  constructor() {
+    this.cleanup = cron.schedule('0 5 * * *', this.dailyCleanup.bind(this));
+    this.cleanup.start();
+  }
+
+  exists(url: string): boolean {
+    return fs.existsSync(this.pathFrom(url));
+  }
+
+  get(url: string): Readable {
+    logger.debug(`get file from cache --> ${url}`);
+    return fs.createReadStream(this.pathFrom(url));
+  }
+
+  put(url: string, stream: Readable) {
+    if (stream.readableLength > 0) {
+      logger.debug(`write file to cache --> ${url}`);
+      const file = fs.createWriteStream(this.pathFrom(url));
+      stream.pipe(file);
+    }
+  }
+
+  dailyCleanup() {
+    fs.readdirSync(CACHE_DIR).forEach( file => {
+      const stat = fs.statSync(path.join(CACHE_DIR, file));
+      const da = moment.duration( moment().diff(moment(stat.atime)) );
+      const dm = moment.duration( moment().diff(moment(stat.mtime)) );
+      logger.debug(`scan file --> ${file} : last access: ${da.humanize()} -- last modified: ${dm.humanize()} -- is cover: ${file.indexOf('cover') != -1}`);
+      // Cover files should leave longer in cache...
+      if (file.indexOf('cover') != -1 && dm.months() > 3) {
+        fs.rmSync(path.join(CACHE_DIR, file));
+      } else if (da.days() > 7 || dm.days() > 0) { // Standard files cached ... image
+        fs.rmSync(path.join(CACHE_DIR, file));
+      }
+
+    });
+  }
+
+  pathFrom(url: string) {
+    const key = encodeURIComponent(url);
+    return path.join(CACHE_DIR, key);
+  }
+}
+
+export default new CacheManager();
